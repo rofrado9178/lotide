1fab3a72d0994876bba2940c465a246b2932fe38
Fransiskus <aldzaidea@gmail.com>
2022-01-10 23:20:57 +0000

    add tail.js and updated head.js

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (array) {
+  let newArr = [];
+  for (let i = 1; i < array.length; i++) {
+    newArr.push(array[i]);
+  }
+  return newArr;
+};
+
+const result = tail([1, 2, 3, 4]);
+assertEqual(result.length, 2);
+assertEqual(result.length, 3);
