b98f5839508c72be8b5f97664688d3d311ba68f6
Fransiskus <aldzaidea@gmail.com>
2022-01-12 20:16:06 +0000

    i added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (words) {
+  let counts = {};
+  const joinWords = words.replace(/\s/g, "");
+
+  for (const word of joinWords) {
+    if (counts[word]) {
+      counts[word] += 1;
+    } else {
+      counts[word] = 1;
+    }
+  }
+  return counts;
+};
