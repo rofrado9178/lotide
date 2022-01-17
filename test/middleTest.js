bb5f46b98196e353bbed00d93ac9a739d0d0aa56
Fransiskus <aldzaidea@gmail.com>
2022-01-17 20:10:59 +0000

    implement mocha and chai for middleTest and tailTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,20 @@
-const { assertArraysEqual } = require("../assertArrayEqual");
+const assert = require("chai").assert;
 const middle = require("../midle");
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("should return middle value from the array if array length is odd numbers", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("should return empty array if array length only 2 or less", () => {
+    assert.deepEqual(middle([1, 2]), []);
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should return two element in the middle of array if array length is even", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail([1, 2, 3, 4]);
-assertEqual(result.length, 2);
-assertEqual(result.length, 3);
+describe("#tail", () => {
+  it("return an array without the head of an element of array", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+
+  it("return an array without the head of an element of array", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+});
